75d935e2b74116c0366f7ebe5d1ff0352d2fb58e
John M. Wargo <johnwargo@users.noreply.github.com>
2023-08-09 17:29:23 -0400

    Update glowing-pumpkin-xiao-bff.ino

--- glowing-pumpkin-xiao-bff.ino
+++ glowing-pumpkin-xiao-bff.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+/*********************************************************
+* Glowing Pumpkin 
+* 
+* Xaio ESP32 S3 version using the Adafruit 5x5 BFF LED
+* Matrix
+*
+* By John M. Wargo
+* https://johnwargo.com
+**********************************************************/
+#include <FastLED.h>
 
+#define NUM_LEDS 25
+#define PIN A3
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  FastLED.addLeds<NEOPIXEL, PIN>(leds, NUM_LEDS);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  leds[0] = CRGB::White;
+  FastLED.show();
+  delay(500);
+  leds[0] = CRGB::Black;
+  FastLED.show();
+  delay(500);
 }
