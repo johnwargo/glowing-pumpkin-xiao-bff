a8560b1e9e8dda64145ee0286c88add5c276ac4d
John M. Wargo <johnwargo@users.noreply.github.com>
2023-08-09 17:43:25 -0400

    Update glowing-pumpkin-xiao-bff.ino

--- glowing-pumpkin-xiao-bff.ino
+++ glowing-pumpkin-xiao-bff.ino
@@ -7,18 +7,29 @@
 * By John M. Wargo
 * https://johnwargo.com
 **********************************************************/
+
 #include <FastLED.h>
 
+#define DEBUG true
 #define NUM_LEDS 25
 #define PIN A3
 
 CRGB leds[NUM_LEDS];
 
 void setup() {
+#ifdef DEBUG
+  Serial.begin(115200);
+  delay(500);
+  Serial.println("Initializing FastLED");
+#endif
   FastLED.addLeds<NEOPIXEL, PIN>(leds, NUM_LEDS);
 }
 
 void loop() {
+#ifdef DEBUG
+  Serial.println("Loop");
+#endif
+
   leds[0] = CRGB::White;
   FastLED.show();
   delay(500);
