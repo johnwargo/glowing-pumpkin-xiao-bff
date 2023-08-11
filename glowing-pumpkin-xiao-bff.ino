f5e9a68dd7db469ea4af448eb1b7b0744eb9e286
John M. Wargo <johnwargo@users.noreply.github.com>
2023-08-11 09:39:22 -0400

    Update glowing-pumpkin-xiao-bff.ino

--- glowing-pumpkin-xiao-bff.ino
+++ glowing-pumpkin-xiao-bff.ino
@@ -14,7 +14,9 @@
 #define NUM_LEDS 25
 #define PIN A3
 
-CRGB leds[NUM_LEDS];
+int numColors = 6;
+uint32_t colors[] = { CRGB::Blue, CRGB::Green, CRGB::Orange, CRGB::Purple, CRGB::Red, CRGB::Yellow };
+CRGB leds[NUM_LEDS];  // LED Array (internal memory structure from FastLED)
 
 void setup() {
 #ifdef DEBUG
@@ -22,18 +24,70 @@ void setup() {
   delay(500);
   Serial.println("Initializing FastLED");
 #endif
+  // Initialize the FastLED library
   FastLED.addLeds<NEOPIXEL, PIN>(leds, NUM_LEDS);
+  delay(500);
+  // Flash the lights RED twice
+  flashLEDs(CRGB::Red, 2, 250);
 }
 
 void loop() {
 #ifdef DEBUG
   Serial.println("Loop");
 #endif
+  //generate a random integer between 1 and 10
+  if ((int)random(11) > 8) {
+    // if it's a 9 or a 10, do that flicker thing
+    flicker();
+  } else {
+    // Otherwise switch to the new color
+    setColor(colors[(int)random(1, numColors + 1)]);
+  }
+}
 
-  leds[0] = CRGB::White;
-  FastLED.show();
-  delay(500);
-  leds[0] = CRGB::Black;
-  FastLED.show();
-  delay(500);
+void flashLEDs(CRGB color, int count, int duration) {
+  // Calculate this only once, no reason to calculate this in the loop
+  int offDuration = duration / 2;
+
+#ifdef DEBUG
+  Serial.println("Flash");
+#endif
+
+  for (int i = 0; i < count; i++) {
+    fill_solid(leds, NUM_LEDS, CRGB::Red);
+    FastLED.show();
+    delay(duration);
+    fill_solid(leds, NUM_LEDS, CRGB::Black);
+    FastLED.show();
+    delay(offDuration);
+  }
+}
+
+void flicker() {
+  // how many times are we going to flash?
+  int flashCount = (int)random(1, 7);
+  //flash the lights in white flashCount times
+  //with a random duration and random delay between each flash
+  for (int i = 0; i < flashCount; i++) {
+    // Set all pixels to white and turn them on
+    setColor(CRGB::White);
+    FastLED.show();
+    // Delay for a random period of time (in milliseconds)
+    delay((int)random(50, 150));
+    //clear the lights (set the color to none)
+    setColor(CRGB::Black);
+    FastLED.show();
+    // Delay for a random period of time (in milliseconds)
+    delay((int)random(100, 500));
+  }
+}
+
+// Fill the NeoPixel array with a specific color
+void setColor(CRGB c) {
+  for (int i = 0; i < 26; i++) {
+    leds[i] = c;
+    FastLED.show();
+    delay(10);
+  }
+  delay((int)random(100, 1000));
 }
